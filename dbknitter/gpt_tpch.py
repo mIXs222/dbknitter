55048b4837799ef6fd634818a54c5be7c4c6278f
chitty-shannon <cshetty2@illinois.edu>
2023-10-17 23:43:58 -0500

    added gpt_tpch - incomplete yet

---
+++ dbknitter/gpt_tpch.py
@@ -0,0 +1,284 @@
+###!!!!!! INCOMPLETE
+
+import requests
+import time
+import datetime
+import openai
+import os
+import argparse
+import sys
+
+os.environ['OPENAI_API_KEY'] = "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"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+MAX_TOKEN=2000
+
+
+
+''' 
+Example Prompt:
+
+- I have a mysql table named DETAILS and a mongoldb table named INTERESTS.
+- DETAILS has the columns NAME, AGE, ADDRESS. INTERESTS has the fields of NAME, INTEREST. INTEREST is a list of strings and can be empty.
+- But the user of my data thinks all data is stored in mysql.
+- They wrote the following query:
+- SELECT DETAILS.AGE, INTERESTS.INTEREST
+- FROM DETAILS JOIN  INTERESTS ON DETAILS.NAME = INTERESTS.NAME
+- Please generate a python code to execute this query. My mysql password is my-pwd. Output of the query must be written to a file name query_output.csv
+'''
+###################################################################################
+
+class SQL_SCHEMA:
+    def __init__(self, primary_key_name):
+        self.primary_key_name = primary_key_name
+        self.columns          = {}                # column_name:[type, null_property, ...]
+
+class SQL_TABLE:
+    def __init__(self, table_name, primary_key_name):
+        self.name = table_name
+        self.schema = SQL_SCHEMA(primary_key_name)   
+
+    def define_schema_from_file(self, filename):
+        ## populate self.schema
+        if filename == "" :
+            print("SQL Table {}: No Schema filename given".format(self.name))
+        else:
+            print("TODO code")  # TODO
+
+    def print_schema(self):
+        pass
+
+
+####################################################################################
+# * Table class represents a generic table in any of the data platforms like mysql, mongodb etc
+# * Properties common to tables in all platforms (like the need for some admin details) is 
+#   to be defined in this class
+# * If we later need to specialize for data platforms, child classes of Table can be defined
+# * Each table needs 2 files: admin details file, equivalent sql schema file
+# TODO: May need a separate platform specific schema file too
+
+class Table:
+    def __init__(self, platform, table_name, primary_key_name, admin_file="", schema_file=""):
+        self.platform       = platform                                        # mysql, mongodb etc
+        self.name           = table_name  
+        self.admin_details  = {}                                              # eg: "Database name" : db_name, "Database password" : sql_pwd etc
+        self.equivalent_sql_table = SQL_TABLE(table_name, primary_key_name)   # A table, no matter which platform, should have an SQL table equivalent,
+                                                                              # which is how it is presented to the buisness analyst (the user). 
+        
+        self.special_case = None                                              # any special rules like how to handle nulls etc
+
+        if self.platform == "mysql":                                          
+            self.column_equivalent = "columns"                                 # what are "columns" called in that platform   
+        elif self.platform == "mongodb":
+            self.column_equivalent = "fields"     
+        else:
+            sys.exit("Invalid platform name. Should be: mysql, mongodb")
+
+        self.define_admin_from_file(admin_file)
+        self.equivalent_sql_table.define_schema_from_file(schema_file)
+
+
+    def define_admin_from_file(self, filename):
+        ## populate self.admin_details
+        if filename == "" :
+            print("Table {}: No admin filename given".format(self.name))
+        else:
+            print("TODO code")  # TODO
+
+
+##################################################################################################
+
+# Collection of all Table objects in my data setup
+
+class Datalake:
+    def __init__(self, name):
+        self.name = name
+        self.tables = {}
+
+
+
+# Prompt for a given Datalake setup
+class Prompt:
+    def __init__(self, datalake, output_file="query_output.csv"):
+        self.datalake      = datalake
+        self.output_file   = output_file
+        self.datalake_info = "I have organized my data as follows: "   ## <table1> in <db1>, ....
+        self.schema_info   = ""                                        ## "<table1> has <columns/fields etc> <cloumn_name>, of type <type>, and can be <special_cases>"
+        self.admin_spec    = "Details of my databases are as follows : "
+        self.story         = "But the user of my data thinks all the data is stored in mysql."
+        self.sql_schema    = "They think the tables have the following schemas : "
+        self.query         = "With that assumption, they wrote the following query: "
+        self.output_spec   = "Generate a python code to execute this query on my original data. Query output should be written to the file {}".format(self.output_file)
+
+#####************************ STOPPED HERE **********************##############
+
+
+class Prompt1:
+    def __init__(self, sql_query):
+        self.data_setup_spec = None
+        self.schema_spec = None
+        self.story_setup = "But the user of my data thinks all data is stored in mysql."
+        self.sql_spec = None # TODO
+        self.sql_query = sql_query
+        self.query_spec = "With that assumption, they wrote the following query: " + self.sql_query
+        self.output_spec = "Generate a python code to execute this query on my original data. Query output should be written to the file <insert>.csv"
+        self.admin_spec = None
+
+    def to_dict(self):
+        member_variables = {attr: getattr(self, attr) for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")}
+        for k, v in member_variables.items():
+            print(f"* {k}: {v}")
+        return member_variables
+
+    def prompting(self, db_objs):
+        temp = "I have a "
+        for i in range(len(db_objs)):
+            temp += f"{db_objs[i].type} table named {db_objs[i].name}"
+            if i < len(db_objs)-1:
+                temp += " and "
+        print(temp)
+        self.data_setup_spec = temp
+        
+        temp = ""
+        for i in range(len(db_objs)):
+            temp += f"{db_objs[i].name} has the columns "
+            for j in range(len(db_objs[i].columns)):
+                temp += f"{db_objs[i].columns[j]} of type {db_objs[i].column_datatype[j]}"
+        self.schema_spec = temp
+        
+        temp = ""
+        for i in range(len(db_objs)):
+            temp += f"Details of my databases '{db_objs[i].name}' are as follows "
+            for admin_d in db_objs[i].admin_detail:
+                temp += admin_d + ", "
+            if i < len(db_objs)-1:
+                temp += " and "
+        self.admin_spec = temp
+        
+class Multi_Message_ChatGPTQuery:
+    def __init__(self):
+        self.messages = list()
+        self.input_message_len = list()
+        self.data = ""
+        self.runtime = -1
+        self.output_text = ""
+        self.gpt_model = "gpt-4" # "gpt-3.5-turbo"
+        self.finished_reason = ""
+        self.response = ""
+        self.created_time = -1
+        self.uid = ""
+        self.completion_tokens = -1
+        self.prompt_tokens = -1
+        self.total_tokens = -1
+        
+    def set_input_message_len(self):
+        assert len(self.input_message_len) == 0
+        for msg in self.messages:
+            self.input_message_len.append(len(msg))
+    
+    def add_context(self, new_msg, role="user"):
+        formatted_msg ={"role": role, "content": new_msg}
+        self.messages.append(formatted_msg)
+
+    def chat_with_gpt(self):
+        ###################################################
+        gpt_response = openai.ChatCompletion.create(
+            model=self.gpt_model,
+            messages=self.messages,
+            temperature=1,
+            max_tokens=MAX_TOKEN,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        ###################################################
+        # TODO: Which one is correct?
+        # self.response = gpt_response.choices[0].text.strip()
+        reason = gpt_response['choices'][0]['finish_reason']
+        if reason != "stop":
+            print("ERROR: GPT failed, finished_reason: {}")
+            print("return None...")
+            return None
+        self.finished_reason = reason
+        self.response = gpt_response['choices'][0]['message']['content']
+        self.created_time = gpt_response["created"]
+        self.uid = gpt_response["id"]
+        self.completion_tokens = gpt_response["usage"]["completion_tokens"]
+        self.prompt_tokens = gpt_response["usage"]["prompt_tokens"]
+        self.total_tokens = gpt_response["usage"]["total_tokens"]
+        print(f"gpt response: {gpt_response}")
+        print(f"extracted response: {self.response}")
+        return self.response
+        
+    def write_result(self):
+        temp = list()
+        assert len(self.messages) == len(self.input_message_len)
+        temp.append(f"uid, {self.uid}")
+        for i in range(len(self.messages)):
+            temp.append(f"message_{i},{self.messages[i]},{self.input_message_len[i]}")
+        temp.append(f"input_message_len,{self.input_message_len}")
+        temp.append(f"MAX_TOKEN, {MAX_TOKEN}")
+        temp.append(f"data, {self.data}")
+        temp.append(f"runtime, {self.runtime}")
+        temp.append(f"output_text, {self.output_text}")
+        temp.append(f"gpt_model, {self.gpt_model}")
+        temp.append(f"finished_reason, {self.finished_reason}")
+        temp.append(f"response, {self.response}")
+        temp.append(f"created_time, {self.created_time}")
+        temp.append(f"completion_tokens, {self.completion_tokens}")
+        temp.append(f"prompt_tokens, {self.prompt_tokens}")
+        temp.append(f"total_tokens, {self.total_tokens}")
+        path_ = util.get_current_time() + "-gpt_output.txt"  # Replace with the path to your file
+        with open(path_, "w") as file:
+            for elem in temp:
+                file.write(elem + "\n")
+    
+    
+class GPT:
+    def __init__(self):
+        self.num_query = 0
+        # self.api_endpoint = 'https://api.openai.com/v1/engines/davinci-codex/completions'
+    
+    def send_request(self, cq):
+        '''
+        reference: https://platform.openai.com/docs/guides/gpt/chat-completions-api
+        The system message helps set the behavior of the assistant. For example, you can modify the personality of the assistant or provide specific instructions about how it should behave throughout the conversation. However note that the system message is optional and the model’s behavior without a system message is likely to be similar to using a generic message such as "You are a helpful assistant."
+        '''
+        cq.set_input_message_len()
+        result = cq.chat_with_gpt()
+        print(result)
+        ts = time.time()
+        # response = requests.post(self.api_endpoint, json=cq.params, headers=cq.headers)
+        # cq.data = response.json() # data is python dictionary. resopnse is json.
+        assert cq.runtime == -1
+        cq.runtime = (time.time() - ts)
+        self.num_query += 1
+        cq.write_result()
+        return cq.response
+    
+        
+    def call_chatgpt_api(self, query, schema):
+        q1 = f"Translate the following SQL query to MongoDB in python api: '{query}'"
+        q2 = f"This is schema of SQL table, '{schema}'"
+        q3 = f"Give me just one python code"
+
+        cq = Multi_Message_ChatGPTQuery()
+        cq.add_context(q1)
+        cq.add_context(q2)
+        cq.add_context(q3)
+        gpt_output = self.send_request(cq)
+        # mongodb_code = gpt_output['choices'][0]['text']
+        print("********************")
+        print("** chatgpt output **")
+        print("********************")
+        print(gpt_output)
+    
+        
+def argparse_add_argument(parser):
+    # parser.add_argument("--query", type=str, default=None, help="sql query that will be used for chatgpt", required=True)
+    return 
+
+if __name__ == "__main__":
+    query = "SELECT CustomerName, City FROM Customers;"
+    schema = "Customer SQL database has columns of CustomerName, City, and Address"
+    gpt = GPT()
+    gpt.call_chatgpt_api(query, schema)
